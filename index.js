0050f024a6098cfc4bacd3dd397ee98dc74cf098
Josh Harris <49429928+jharris711@users.noreply.github.com>
2020-05-20 01:02:13 -0400

    fix

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express     = require('express')
 const app         = express()
 const indexRoutes = require("./routes/index");
-const auto_reply  = require('./dms/auto_reply_dm');
 const port        = process.env.PORT || 80;
 //const port      = 3000;
 require('dotenv').config();
 
 app.set("view engine", "ejs");
 //app.use(express.static(__dirname + "/public"));
-
-
-//Auto-reply to DMs:
-auto_reply.auto_reply_to_dms()
 
 
 //Index route for status
